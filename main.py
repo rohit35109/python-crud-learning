8b52de0d1ae5b4d29bdba638a2b7b0cf76e9e473
Rohit Kumar S <reach@rohitkumardubey.com>
2025-07-22 12:47:59 +0530

    Completed with stuents list, put and get apis

--- main.py
+++ main.py
@@ -1,12 +1,39 @@
-from fastapi import FastAPI
-from typing import Union
+from fastapi import FastAPI, HTTPException
+from typing import List
+from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return { "Hello": "World" }
+students = [];
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return { "item_id": item_id, "q": q }
+class Student(BaseModel):
+    id: int
+    name: str
+    age: int
+    grade: str
+
+@app.get("/students")
+def get_students():
+    return { "students": students }
+
+@app.post('/students')
+def create_student(student: Student):
+    students.append(student)
+    return { "message": "Student Added", "student": student }
+
+@app.get('/student/{id}')
+def get_student(student_id: int):
+    for student in students:
+        if student.id == student_id:
+            return student
+    #if not found
+    raise HTTPException(status_code=404, detail="Student not found")
+
+@app.put('/student/{id}')
+def update_student(id: int, updated_student: Student):
+    print(id, updated_student)
+    for index, student in enumerate(students):
+        if student.id == id:
+            students[index] = updated_student
+            return { "message": "Student Updated", "student": updated_student }
+    raise HTTPException(status_code=404, detail="Student not found")
